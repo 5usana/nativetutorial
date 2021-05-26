95dd7813ab572d6b65317701e9f756f77dfd0985
Susana Martins <scmartins87@gmail.com>
2021-05-26 16:50:59 -0700

    added Text, and played with styles for header, body, and boldText

--- App.js
+++ App.js
@@ -5,7 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldtext}>HEADER</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>My first React Native tutorial. I think this is pretty neat!</Text>
+        <Text> Gibberish gibberish gibberish gibberish gibberish </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +24,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'coral',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  }
 });
+
