64094f6ae3a487b771b5e50d15d9ae7915752f69
Susana Martins <scmartins87@gmail.com>
2021-05-27 13:50:15 -0700

    cleared App.js to start Lists and Scroll tutorial

--- App.js
+++ App.js
@@ -1,40 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default function App() {
-  const [name, setName] = useState('Susana');
-  const [age, setAge] = useState('34');
-  // const [person, setPerson] = useState({ name: 'Mario', age: 40 });
 
-  // const clickHandler = () => {
-  //   setName('Charlie');
-  //   setPerson({ name: 'Luigi', age: 45 })
-  // }
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput 
-        style={styles.input}
-        placeholder='e.g. John Doe'
-        onChangeText={(val) => setName(val)} />
+      
+      
+      
 
-      <Text>Enter age:</Text>
-      <TextInput 
-        style={styles.input}
-        placeholder='e.g. 37'
-        onChangeText={(val) => setAge(val)} />
-
-      <Text>name: {name}, age: {age}</Text>
-
-
-        {/* <Text>My name is {name}</Text>
-        <Text>His name is {person.name} and his age is {person.age}</Text>
-        <View style={styles.buttonContainer}>
-          <Button title='update state' onPress={clickHandler}/>
-        </View>
-      <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -46,15 +20,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  }
-  // buttonContainer: {
-  //   marginTop:20
-  // },
 });
 
