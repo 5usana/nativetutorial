1658b08919dabcfa52f28482a82761398c1078a5
Susana Martins <scmartins87@gmail.com>
2021-05-27 13:57:43 -0700

    returned list items by mapping through people state

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
+export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'shaun', key: '1' },
+    { name: 'yoshi', key: '2' },
+    { name: 'mario', key: '3' },
+    { name: 'luigi', key: '4' },
+    { name: 'peach', key: '5' },
+    { name: 'toad', key: '6' },
+    { name: 'bowser', key: '7' },
+  ]);
 
 
   return (
     <View style={styles.container}>
       
-      
+    { people.map((item) => {
+      return (
+        <View>
+          <Text>{item.name}</Text>
+        </View>
+      )
+    }) }  
       
 
     </View>
@@ -17,8 +33,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
 
