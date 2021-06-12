f3199ef17917da0732b6b848b56597347a267eea
Susana Martins <scmartins87@gmail.com>
2021-06-11 17:38:23 -0700

    added useState for todos/setTodos

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ToolbarAndroidBase } from 'react-native';
 
 export default function App() {
+const [todos, setTodos] = useState([
+  { text: 'buy coffee', key: '1' },
+  { text: 'create an app', key: '2' },
+  { text: 'play on the switch', key: '3' }
+]);
+
+
+
   return (
     <View style={styles.container}>
+      2
       {/* header */}
       <View style={styles.content}>
         {/* to form */}
         <View style={styles.list}>
-
+          <FlatList 
+          
+          />
         </View>
     </View>
     </View>
