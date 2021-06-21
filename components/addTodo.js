b77d78e8a7e3a5087e6c7edc9c1eec082d099b22
Susana Martins <scmartins87@gmail.com>
2021-06-20 20:52:51 -0700

    added addTodo component

---
+++ components/addTodo.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, Button } from 'react-native';
+
+export default function AddTodo() {
+    return(
+        <View></View>
+    )
+}
